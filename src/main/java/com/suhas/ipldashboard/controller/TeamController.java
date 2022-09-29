d49d7c8f170261ca2a486725b32c35109035b71e
Suhas <smsuhas2@gmail.com>
2022-09-29 23:22:42 +0530

    Added page in React

--- src/main/java/com/suhas/ipldashboard/controller/TeamController.java
+++ src/main/java/com/suhas/ipldashboard/controller/TeamController.java
@@ -1,6 +1,8 @@
 package com.suhas.ipldashboard.controller;
 
 import java.util.Optional;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import com.suhas.ipldashboard.model.Team;
 import com.suhas.ipldashboard.repository.MatchRepository;
 import com.suhas.ipldashboard.repository.TeamRepository;
 
+@CrossOrigin
 @RestController
 public class TeamController {
 
